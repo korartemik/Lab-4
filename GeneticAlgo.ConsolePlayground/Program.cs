6fe1ba8dbfafd7981f1c691a4c405a8395ce2da2
Artem Korchagin <kor.artemik@yandex.ru>
2022-05-19 19:07:42 +0300

    add gen alg

--- GeneticAlgo.ConsolePlayground/Program.cs
+++ GeneticAlgo.ConsolePlayground/Program.cs
@@ -2,13 +2,21 @@
 
 using GeneticAlgo.Shared;
 using GeneticAlgo.Shared.Tools;
+using GeneticAlgo.Core;
+using GeneticAlgo.Core.Models;
+using GeneticAlgo.Shared.Models;
 using Serilog;
 
-Logger.Init();
-Log.Information("Start console polygon");
-var dummyExecutionContext = new DummyExecutionContext(10, 10, 3);
-dummyExecutionContext.Reset();
-await dummyExecutionContext.ExecuteIterationAsync();
-Log.Information("Polygon end");
-
-Console.WriteLine("Hello, World!");
+GenAlgoritm geneticAlgo = new GenAlgoritm("j");
+geneticAlgo.Next();
+Individual individ = geneticAlgo.GetBest().First();
+while (individ.Survival < 0.99)
+{
+    geneticAlgo.Next();
+    individ = geneticAlgo.GetBest().First();
+}
+foreach(Point point in individ.Points)
+{
+    Console.WriteLine(point.X + " " + point.Y);
+}
+Console.WriteLine(individ.Survival);
---
+++ GeneticAlgo.Core/Models/Individual.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneticAlgo.Shared.Models;
+
+namespace GeneticAlgo.Core.Models
+{
+    public class Individual
+    {
+
+        public Individual(List<Point> points)
+        {
+            Points = points;
+        }
+
+        public List<Point> Points { get; private set; }
+        public double Survival { get; set; }
+    }
+}
